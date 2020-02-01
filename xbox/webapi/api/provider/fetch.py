1ce77d00e0495cc2aa4b594905b8b7c3c78c145e
Kai van Rijswijk de Leeuw van Weenen <support@belimitless.nl>
2020-02-01 14:56:19 +0100

    Update fetch.py

--- xbox/webapi/api/provider/fetch.py
+++ xbox/webapi/api/provider/fetch.py
@@ -6,11 +6,7 @@ from xbox.webapi.api.provider.baseprovider import BaseProvider
 
 
 class FetchProvider(BaseProvider):
-    ACHIEVEMENTS_URL = "https://achievements.xboxlive.com"
-    HEADERS_GAME_360_PROGRESS = {'x-xbl-contract-version': '1'}
-    HEADERS_GAME_PROGRESS = {'x-xbl-contract-version': '2'}
-
-    def endpoint(self, url):
+    def endpoint(self, url, xbl_headers):
         """
         Fetch custom Xbox API endpoint
 
@@ -20,4 +16,4 @@ class FetchProvider(BaseProvider):
         Returns:
             :class:`requests.Response`: HTTP Response
         """
-        return self.client.session.get(url, headers=self.HEADERS_GAME_PROGRESS)
+        return self.client.session.get(url, headers=xbl_headers)
